f8b9eea2f06299d80431ec914bd957a5bafe89f3
nirav <niravnikhil@gmail.com>
2025-03-22 23:25:46 +0545

    add call start and hangup functions, reset chat on restart

--- embed-script/src/Call.ts
+++ embed-script/src/Call.ts
@@ -4,11 +4,13 @@ import { Chat, ChatEntry } from "./Chat";
 export class Call {
   private client: SignalWireClient | null = null;
   private callDetails: CallDetails | null = null;
-  chat: Chat = new Chat();
+  chat: Chat | null = null;
+  currentCall: Call | null = null;
 
   constructor(callDetails: CallDetails) {
     this.callDetails = callDetails;
     this.setupClient();
+    this.chat = new Chat();
   }
 
   async setupClient() {
@@ -24,7 +26,7 @@ export class Call {
       console.log(params);
       // AI partial result (typing indicator)
       console.log("ai.partial_result", params.text);
-      this.chat.handleEvent(
+      this.chat?.handleEvent(
         "ai.partial_result",
         params.text ?? "",
         params.barged ?? false
@@ -37,7 +39,7 @@ export class Call {
       // AI speech detection (user speaking)
       const cleanText = params.text.replace(/\{confidence=[\d.]+\}/, "");
       console.log("ai.speech_detect", cleanText);
-      this.chat.handleEvent(
+      this.chat?.handleEvent(
         "ai.speech_detect",
         cleanText,
         params.type !== "normal" // this also doesn't seem to have type=barged, but leaving here for now
@@ -48,7 +50,7 @@ export class Call {
     this.client.on("ai.completion", (params) => {
       console.log(params);
       // AI completion (final response)
-      this.chat.handleEvent(
+      this.chat?.handleEvent(
         "ai.completion",
         params.text ?? "",
         params.type === "barged"
@@ -60,7 +62,7 @@ export class Call {
       console.log(params);
       // AI response utterance (spoken response)
       console.log("ai.response_utterance", params.utterance);
-      this.chat.handleEvent(
+      this.chat?.handleEvent(
         "ai.response_utterance",
         params.utterance ?? "",
         false // this doesn't have barged yet
@@ -79,21 +81,38 @@ export class Call {
       throw new Error("Call details are not set");
     }
 
-    const currentCall = await this.client.dial({
+    const currentCallLocal = await this.client.dial({
       to: this.callDetails.destination,
       rootElement: container,
       audio: this.callDetails.supportsAudio,
       video: this.callDetails.supportsVideo,
       negotiateVideo: this.callDetails.supportsVideo,
     });
-    currentCall.start();
+    // @ts-expect-error FabricRoomSession missing types
+    this.currentCall = currentCallLocal;
 
-    this.chat.onUpdate = () => {
-      onChatChange(this.chat.getHistory());
-    };
+    if (this.chat) {
+      this.chat.onUpdate = () => {
+        onChatChange(this.chat?.getHistory() ?? []);
+      };
+    }
 
-    console.log("currentCall", currentCall);
+    console.log("currentCall", currentCallLocal);
 
-    return currentCall;
+    return currentCallLocal;
+  }
+
+  async start() {
+    if (!this.client) {
+      throw new Error("Client is not set");
+    }
+    await this.currentCall?.start();
+  }
+
+  async hangup() {
+    if (!this.currentCall) {
+      throw new Error("Call is not set");
+    }
+    await this.currentCall?.hangup();
   }
 }
